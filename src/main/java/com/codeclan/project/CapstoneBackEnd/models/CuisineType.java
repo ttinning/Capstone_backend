ba92caaac6519b87123187b87a1b846e4394aaff
Tristan Tinning <tristan_tinning@hotmail.com>
2021-08-12 11:53:43 +0100

    backend Load

---
+++ src/main/java/com/codeclan/project/CapstoneBackEnd/models/CuisineType.java
@@ -0,0 +1,13 @@
+package com.codeclan.project.CapstoneBackEnd.models;
+
+public enum CuisineType {
+    INDIAN,
+    ITALIAN,
+    THAI,
+    MEXICAN,
+    VIETNAMESE,
+    SPANISH,
+    LEBANESE,
+    JAPANESE,
+    BRITISH;
+}
